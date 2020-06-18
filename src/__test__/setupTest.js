e63bad7210573ed35cdacb0a96392a3c2cfc0ce8
hugo <hugoandresdiazbernal@gmail.com>
2020-06-18 17:45:11 -0500

    Travis CI

--- src/__test__/setupTest.js
+++ src/__test__/setupTest.js
@@ -1,8 +1,8 @@
-import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
 
 
 configure({ adapter: new Adapter() });
 
-
+global.fetch = require('jest-fetch-mock');
 
